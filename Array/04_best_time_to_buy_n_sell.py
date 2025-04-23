c90c0ea5fe50907772ef8e47cc6a3823b0d7712c
mesbah <mesbah0010@gmail.com>
2025-04-23 22:33:57 +0600

    update

--- Array/04_best_time_to_buy_n_sell.py
+++ Array/04_best_time_to_buy_n_sell.py
@@ -1,5 +1,6 @@
 prices = [7,1,5,3,6,4]
 
+'''
 min_price = float('inf')
 max_profit = 0        
         
@@ -10,3 +11,23 @@ for price in prices:
     max_profit = max(profit, max_profit)
                 
 print(max_profit)
+
+'''
+# Time: O(n)
+# Space: O(1)
+
+min_price = float('inf')
+max_profit = 0        
+        
+for price in prices:
+    if price < min_price:
+        min_price = price
+            
+    profit = price - min_price
+        
+    if profit > max_profit:
+        max_profit = profit
+
+print(max_profit)
+
+                
