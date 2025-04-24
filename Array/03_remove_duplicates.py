58a03ace990ce83d3f4d29cb98c25b3decd538f5
mesbah <mesbah0010@gmail.com>
2025-04-24 10:01:58 +0600

    update

--- Array/03_remove_duplicates.py
+++ Array/03_remove_duplicates.py
@@ -1,3 +1,14 @@
+'''
+26. Remove Duplicates from Sorted Array
+
+Example 2:
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+'''
+
 nums = [0,0,1,1,1,2,2,3,3,4]
 
 # remove_duplicates = set(nums)
@@ -5,50 +16,13 @@ nums = [0,0,1,1,1,2,2,3,3,4]
 # print(remove_duplicates,type(remove_duplicates))
 # print(nums)
 
-# n= len(nums)
-# r = 1
-
-# for i in range(1,n):
-#     if nums[i] != nums[i-1]:
-#         nums[r] = nums[i]
-#         r += 1
-# print(r)
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-nums = [0,0,1,1,1,2,2,3,3,4]
-
-n = len(nums)
+n= len(nums)
 r = 1
 
-for l in range(1,n):
-    if nums[l] != nums[r-1]:
-        nums[r] = nums[l]
+for i in range(1,n):
+    if nums[i] != nums[i-1]:
+        nums[r] = nums[i]
         r += 1
-
 print(r)
 
 
